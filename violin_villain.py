1affa56bb3dcae9c35de1f9f6227ab3b5fd851ef
Derek Jones <derekjones@asu.edu>
2022-12-05 18:47:35 -0500

    Reordered note table

--- violin_villain.py
+++ violin_villain.py
@@ -2,6 +2,7 @@ import crepe
 import sounddevice
 import math
 import pygame
+from collections import OrderedDict
 
 A4 = 440
 MUSICAL_NOTES = ['A', 'Bb', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#']
@@ -22,41 +23,42 @@ def get_frequency_percentage(freq):
   position += (NUM_NOTES*NUM_OCTAVES/2)
   return position/(NUM_NOTES*NUM_OCTAVES)
 
-def generate_frequency_dict():
+def generate_note_dict():
   starting_note = A4
-  frequency_dict = {}
+  note_dict = OrderedDict()
   #calculate [a0,a7)
   #starting value is -48 (the number of steps from a1 to a4
   exponent = -60
   for i in range(NUM_OCTAVES):
     for j in range(NUM_NOTES):
       note_sub_offset = i + int(j>=3) - 1
-      frequency_dict[starting_note*pow(pow(2,1/12),exponent)] = MUSICAL_NOTES[j] + str(note_sub_offset)
+      note_dict[MUSICAL_NOTES[j] + str(note_sub_offset)] = {"frequency":starting_note*pow(pow(2,1/12),exponent),
+                                                            "position_to_a4":exponent,
+                                                            "position_on_staff":-1}
       exponent +=1
-  return frequency_dict
+  return
 
-def draw_staff():
-  scalar = get_frequency_percentage(329.63) #E4
+def draw_staff(note_dict):
+  scalar = get_frequency_percentage(note_dict["E4"]["frequency"])
   #flip Y
   pygame.draw.line(screen, (0, 0, 0), (0,  GAME_Y - (GAME_Y * scalar)), (GAME_X, GAME_Y - (GAME_Y * scalar)), 5)
 
-  scalar = get_frequency_percentage(392.00) #G4
+  scalar = get_frequency_percentage(note_dict["G4"]["frequency"])
+  pygame.draw.line(screen, (0, 0, 0), (0,  GAME_Y - (GAME_Y * scalar)), (GAME_X, GAME_Y - (GAME_Y * scalar)), 5)
+
+  scalar = get_frequency_percentage(note_dict["B4"]["frequency"])
   #flip Y
   pygame.draw.line(screen, (0, 0, 0), (0,  GAME_Y - (GAME_Y * scalar)), (GAME_X, GAME_Y - (GAME_Y * scalar)), 5)
 
-  scalar = get_frequency_percentage(493.88) #B4
+  scalar = get_frequency_percentage(note_dict["D5"]["frequency"])
   #flip Y
   pygame.draw.line(screen, (0, 0, 0), (0,  GAME_Y - (GAME_Y * scalar)), (GAME_X, GAME_Y - (GAME_Y * scalar)), 5)
 
-  scalar = get_frequency_percentage(587.33) #D5
+  scalar = get_frequency_percentage(note_dict["F5"]["frequency"])
   #flip Y
   pygame.draw.line(screen, (0, 0, 0), (0,  GAME_Y - (GAME_Y * scalar)), (GAME_X, GAME_Y - (GAME_Y * scalar)), 5)
 
-  scalar = get_frequency_percentage(698.46) #F5
-  #flip Y
-  pygame.draw.line(screen, (0, 0, 0), (0,  GAME_Y - (GAME_Y * scalar)), (GAME_X, GAME_Y - (GAME_Y * scalar)), 5)
-
-def gameloop(frequency_dict):
+def gameloop(note_dict, frequency_to_note_dict):
   # Did the user click the window close button?
   for event in pygame.event.get():
       if event.type == pygame.QUIT:
@@ -64,23 +66,23 @@ def gameloop(frequency_dict):
 
   screen.fill((255,255,255))
 
-  draw_staff()
+  draw_staff(note_dict)
 
-  freq, closest_freq, confidence = get_frequency_from_microphone(frequency_dict)
+  freq, closest_note, confidence = get_frequency_from_microphone(note_dict, frequency_to_note_dict)
 
   if confidence > 0.40:
     position_scalar = get_frequency_percentage(freq)
     #flip y
     pygame.draw.circle(screen, (0, 0, 255), (100, GAME_Y - (GAME_Y * position_scalar)), 10)
 
-    print(f'{frequency_dict[closest_freq]}, {get_percent_note_freq_delta(freq, closest_freq)}, {confidence}, {position_scalar}')
+    print(f'{closest_note}, {get_percent_note_freq_delta(freq, note_dict[closest_note]["frequency"])}, {confidence}, {position_scalar}')
   else:
     print("nothing heard")
 
   pygame.display.flip()
   return True
 
-def get_frequency_from_microphone(frequency_dict):
+def get_frequency_from_microphone(note_dict, frequency_to_note_dict):
   fs = 16000
   duration = 0.1 #seconds, match with step size~
   recording = sounddevice.rec(int(duration * fs), samplerate=fs, channels=2,dtype='float64')
@@ -91,30 +93,33 @@ def get_frequency_from_microphone(frequency_dict):
   # find the last frequency smaller than the recorded frequency
   freq = frequency[0]
   index = 0
-  smaller_freq = 0
-  larger_freq = 0
-  for k in frequency_dict.keys():
+  smaller_note = -1
+  larger_note = -1
+  for k, v in frequency_to_note_dict.items():
     if k < freq:
-      smaller_freq = k
+      smaller_note = v
       continue
     else:
-      larger_freq = k
+      larger_note = v
       break
   #now see which is closer
-  if smaller_freq > 0:
-    closest_freq = smaller_freq if abs(get_percent_note_freq_delta(freq, smaller_freq)) < \
-        abs(get_percent_note_freq_delta(freq, larger_freq)) else larger_freq
+  if smaller_note > 0:
+    closest_note = smaller_note if abs(get_percent_note_freq_delta(freq, note_dict[smaller_note]["frequency"])) < \
+        abs(get_percent_note_freq_delta(freq, note_dict[larger_note]["frequency"])) else larger_note
   else:
-    closest_freq = larger_freq
+    closest_note = larger_note
 
-  return freq, closest_freq, confidence[0]
+  return freq, closest_note, confidence[0]
 
 if __name__ == "__main__":
-  frequency_dict = generate_frequency_dict()
+  note_dict = generate_note_dict()
+  frequency_to_note_dict = OrderedDict()
+  for k,v in note_dict.items():
+    frequency_to_note_dict[v["frequency"]] = k
   pygame.init()
   screen = pygame.display.set_mode([GAME_X, GAME_Y])
 
-  while gameloop(frequency_dict):
+  while gameloop(note_dict, frequency_to_note_dict):
     pass
 
   pygame.quit()
