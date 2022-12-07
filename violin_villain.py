a86b0bb92dbf987bd24e5c7d75a01ac175ab4e67
Derek Jones <derekjones@asu.edu>
2022-12-06 23:23:14 -0500

    more work on scaling

--- violin_villain.py
+++ violin_villain.py
@@ -90,14 +90,14 @@ def generate_note_dict():
   return note_dict
 
 def draw_sharp(color, center_x, center_y):
-  pygame.draw.line(screen, color, (center_x - WORLD_SCALAR * 3, center_y + WORLD_SCALAR * 7), (center_x - WORLD_SCALAR * 1, center_y - WORLD_SCALAR * 7), WORLD_SCALAR * 2)
-  pygame.draw.line(screen, color, (center_x + WORLD_SCALAR * 1, center_y + WORLD_SCALAR * 7), (center_x + WORLD_SCALAR * 3, center_y - WORLD_SCALAR * 7), WORLD_SCALAR * 2)
-  pygame.draw.line(screen, color, (center_x - WORLD_SCALAR * 5, center_y - WORLD_SCALAR * 1), (center_x + WORLD_SCALAR * 5, center_y - WORLD_SCALAR * 3), WORLD_SCALAR * 4)
-  pygame.draw.line(screen, color, (center_x - WORLD_SCALAR * 5, center_y + WORLD_SCALAR * 3), (center_x + WORLD_SCALAR * 5, center_y + WORLD_SCALAR * 1), WORLD_SCALAR * 4)
+  pygame.draw.line(screen, color, (center_x - WORLD_SCALAR * 3, center_y + WORLD_SCALAR * 7), (center_x - WORLD_SCALAR * 1, center_y - WORLD_SCALAR * 7), WORLD_SCALAR * 1)
+  pygame.draw.line(screen, color, (center_x + WORLD_SCALAR * 1, center_y + WORLD_SCALAR * 7), (center_x + WORLD_SCALAR * 3, center_y - WORLD_SCALAR * 7), WORLD_SCALAR * 1)
+  pygame.draw.line(screen, color, (center_x - WORLD_SCALAR * 5, center_y - WORLD_SCALAR * 1), (center_x + WORLD_SCALAR * 5, center_y - WORLD_SCALAR * 3), WORLD_SCALAR * 2)
+  pygame.draw.line(screen, color, (center_x - WORLD_SCALAR * 5, center_y + WORLD_SCALAR * 3), (center_x + WORLD_SCALAR * 5, center_y + WORLD_SCALAR * 1), WORLD_SCALAR * 2)
 
 def draw_flat(color, center_x, center_y):
-  pygame.draw.line(screen, color, (center_x - WORLD_SCALAR * 4, center_y + WORLD_SCALAR * 7), (center_x - WORLD_SCALAR * 4, center_y - WORLD_SCALAR * 9), WORLD_SCALAR * 2)
-  pygame.draw.arc(screen, color, pygame.Rect(center_x - WORLD_SCALAR * 7, center_y - WORLD_SCALAR * 1, WORLD_SCALAR * 14, WORLD_SCALAR * 7), 1.75*math.pi, 2.5*math.pi, WORLD_SCALAR * 4)
+  pygame.draw.line(screen, color, (center_x - WORLD_SCALAR * 4, center_y + WORLD_SCALAR * 7), (center_x - WORLD_SCALAR * 4, center_y - WORLD_SCALAR * 7), WORLD_SCALAR * 2)
+  pygame.draw.arc(screen, color, pygame.Rect(center_x - WORLD_SCALAR * 7, center_y - WORLD_SCALAR * 1, WORLD_SCALAR * 10, WORLD_SCALAR * 7), 1.75*math.pi, 2.5*math.pi, WORLD_SCALAR * 4)
   pygame.draw.line(screen, color, (center_x-WORLD_SCALAR * 4, center_y + WORLD_SCALAR * 7), (center_x, center_y + WORLD_SCALAR * 3), WORLD_SCALAR * 3)
   pygame.draw.line(screen, color, (center_x-WORLD_SCALAR * 4, center_y + WORLD_SCALAR * 1), (center_x - WORLD_SCALAR , center_y), WORLD_SCALAR * 3)
 
